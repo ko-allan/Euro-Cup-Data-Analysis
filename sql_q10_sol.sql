91f6202e9603d03fdd7a1e767b6eaaab464842ab
Allan Villanueva <84660320+ko-allan@users.noreply.github.com>
2022-04-26 17:35:36 -0700

    Update sql_q10_sol.sql
    Improved readability

--- sql_q10_sol.sql
+++ sql_q10_sol.sql
@@ -1,6 +1,6 @@
 SELECT
 	a.*,
-    b.country_name
+	b.country_name
 FROM euro_cup_2016.player_mast AS a
 INNER JOIN euro_cup_2016.soccer_country AS b ON a.team_id = b.country_id
 WHERE country_name = 'England'
